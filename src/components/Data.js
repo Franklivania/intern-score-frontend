9556a274f452cdbfe128952e0ba3c026c03735a5
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-02-19 22:13:14 +0100

    sucessfully integerated API

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,28 +2,20 @@ import React, { useEffect, useState } from 'react'
 import './Data.css'
 
 const Data = () => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    fetch("https://sheetlabs.com/1313/chibuzofrontend" , {
-        method: 'GET',
-        headers: {
+    const endpoint = "https://sheetlabs.com/1313/chibuzofrontend"
+    const headers = {
         'Content-Type': 'application/json',
-        
-    },
-    })
-    .then(response => {
-    if (response.status === 200) {
-        return response.json();
-    } else {
-        throw new Error('Something went wrong');
-    }
-    })
-    .then(data => console.log(data))
-    .catch((error) => {
-        console.error(error);
-    });
+    };
+
+    fetch(endpoint, { headers })
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(error => console.error(error));
   }, [])
+    
 
   if (!data) {
     return <div>Loading Data...</div>
@@ -31,7 +23,32 @@ const Data = () => {
 
   return (
     <div className='data'>
-      {data}
+        <div className="wrapper">
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th >Full Name</th>
+                    <th>Attendance 100</th>
+                    <th>Power Ranking 3000</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.Rank}</td>
+                        <div className="name">
+                            <td>{item.FirstName}</td>
+                            <td>{item.LastName}</td>
+                        </div>
+                        <td className='attendance'>{item.Attendance100}</td>
+                        <td>{item.PowerRanking3000}</td>
+                    </tr>
+                ))}
+            </tbody>
+      </table>
+    </div>
     </div>
   )
 }
